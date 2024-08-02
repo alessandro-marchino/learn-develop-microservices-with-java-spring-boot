225214cac8ba7f11814db57de16cb0a96f0b0191
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-08-02 11:01:53 +0200

    Lesson 85

--- loans/src/main/java/com/eazybytes/loans/dto/LoanContactInfoDto.java
+++ loans/src/main/java/com/eazybytes/loans/dto/LoanContactInfoDto.java
@@ -5,24 +5,25 @@ import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
 @ConfigurationProperties(prefix = "loans")
 @Schema(name = "Loan", description = "Schema to hold Contact information")
-public record LoanContactInfoDto(
-		@Schema(description = "Loan contact information message", example = "Welcome")
-		String message,
-		@Schema(description = "Contact details")
-		ContactDetailsDto contactDetails,
-		@Schema(description = "On-call support numbers", example = "[ (000) 000-1234 ]")
-		List<String> onCallSupport) {
-	// Nothing
+@Data
+public class LoanContactInfoDto {
+	@Schema(description = "Loan contact information message", example = "Welcome")
+	private String message;
+	@Schema(description = "Contact details")
+	private ContactDetailsDto contactDetails;
+	@Schema(description = "On-call support numbers", example = "[ (000) 000-1234 ]")
+	private List<String> onCallSupport;
 }
 
 @Schema(name = "ContactDetails", description = "Schema to hold Contact Details information")
-record ContactDetailsDto(
-		@Schema(description = "Contact Name", example = "John Doe")
-		String name,
-		@Schema(description = "Contact Email", example = "john@example.com")
-		String email) {
-	// Nothing
+@Data
+class ContactDetailsDto {
+	@Schema(description = "Contact Name", example = "John Doe")
+	private String name;
+	@Schema(description = "Contact Email", example = "john@example.com")
+	private String email;
 }
