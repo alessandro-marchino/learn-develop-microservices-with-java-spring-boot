7b2bf9a83fd483e3a4e087d0c99b2ef89a2f93e7
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-08-09 15:35:52 +0200

    Lesson 127

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -1,5 +1,7 @@
 package com.eazybytes.gatewayserver;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -16,19 +18,25 @@ public class GatewayserverApplication {
 	@Bean
 	RouteLocator eazyBankRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(p -> p
-						.path("/eazybank/accounts/**")
-						.filters(f -> f.rewritePath("/eazybank/accounts/?(?<segment>.*)", "/${segment}"))
-						.uri("lb://ACCOUNTS"))
-				.route(p -> p
-						.path("/eazybank/cards/**")
-						.filters(f -> f.rewritePath("/eazybank/cards/?(?<segment>.*)", "/${segment}"))
-						.uri("lb://CARDS"))
-				.route(p -> p
-						.path("/eazybank/loans/**")
-						.filters(f -> f.rewritePath("/eazybank/loans/?(?<segment>.*)", "/${segment}"))
-						.uri("lb://LOANS"))
-				.build();
+			.route(p -> p
+				.path("/eazybank/accounts/**")
+				.filters(f -> f
+					.rewritePath("/eazybank/accounts/?(?<segment>.*)", "/${segment}")
+					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+				.uri("lb://ACCOUNTS"))
+			.route(p -> p
+				.path("/eazybank/cards/**")
+				.filters(f -> f
+					.rewritePath("/eazybank/cards/?(?<segment>.*)", "/${segment}")
+					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+				.uri("lb://CARDS"))
+			.route(p -> p
+				.path("/eazybank/loans/**")
+				.filters(f -> f
+					.rewritePath("/eazybank/loans/?(?<segment>.*)", "/${segment}")
+					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+				.uri("lb://LOANS"))
+			.build();
 	}
 
 }
