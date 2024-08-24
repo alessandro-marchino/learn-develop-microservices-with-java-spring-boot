21c0c5bae33d9cbd70cfaf2d0e45d821ded12929
amarchino <marchino.alessandro@gmail.com>
2024-08-24 20:45:14 +0200

    Lesson 203

---
+++ message/src/main/java/com/eazybytes/message/dto/AccountMessageDto.java
@@ -0,0 +1,5 @@
+package com.eazybytes.message.dto;
+
+public record AccountMessageDto(Long accountNumber, String name, String email, String mobileNumber) {
+
+}
---
+++ message/src/main/java/com/eazybytes/message/function/MessageFunctions.java
@@ -0,0 +1,8 @@
+package com.eazybytes.message.function;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageFunctions {
+
+}
