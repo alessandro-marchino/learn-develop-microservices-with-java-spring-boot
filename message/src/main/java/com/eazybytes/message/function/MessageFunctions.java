600f6ae33362e7601a8d77ba3893475861c9e8ed
amarchino <marchino.alessandro@gmail.com>
2024-08-24 23:30:16 +0200

    Lesson 204

--- message/src/main/java/com/eazybytes/message/function/MessageFunctions.java
+++ message/src/main/java/com/eazybytes/message/function/MessageFunctions.java
@@ -1,8 +1,31 @@
 package com.eazybytes.message.function;
 
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.eazybytes.message.dto.AccountMessageDto;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class MessageFunctions {
 
+	@Bean
+	Function<AccountMessageDto, AccountMessageDto> email() {
+		return accountMessageDto -> {
+			log.info("Sending email with the details: {}", accountMessageDto.toString());
+			return accountMessageDto;
+		};
+	}
+
+	@Bean
+	Function<AccountMessageDto, Long> sms() {
+		return accountMessageDto -> {
+			log.info("Sending SMS with the details: {}", accountMessageDto.toString());
+			return accountMessageDto.accountNumber();
+		};
+	}
 }
