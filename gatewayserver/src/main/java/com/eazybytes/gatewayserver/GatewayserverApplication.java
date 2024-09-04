23ab26082e22b692d50c3f23a8302628acdb995c
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-09-04 10:31:28 +0200

    Lesson 256

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -20,6 +21,7 @@ import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
@@ -37,7 +39,7 @@ public class GatewayserverApplication {
 					.circuitBreaker(config -> config
 						.setName("accountsCircuitBreaker")
 						.setFallbackUri("forward:/contactSupport")))
-				.uri("lb://ACCOUNTS"))
+				.uri("http://accounts:8080"))
 			.route(p -> p
 				.path("/eazybank/cards/**")
 				.filters(f -> f
@@ -46,7 +48,7 @@ public class GatewayserverApplication {
 					.requestRateLimiter(config -> config
 							.setRateLimiter(redisRateLimiter())
 							.setKeyResolver(userKeyResolver())))
-				.uri("lb://CARDS"))
+				.uri("http://cards:9000"))
 			.route(p -> p
 				.path("/eazybank/loans/**")
 				.filters(f -> f
@@ -56,7 +58,7 @@ public class GatewayserverApplication {
 						.setRetries(3)
 						.setMethods(HttpMethod.GET)
 						.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
-				.uri("lb://LOANS"))
+				.uri("http://loans:8090"))
 			.build();
 	}
 
